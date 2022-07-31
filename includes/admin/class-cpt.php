552447ea2ea5d2c4289addc0c29969ad07e286e3
pratik028 <pratik_deshmukh28@yahoo.com>
2022-07-31 12:10:43 +0530

    Updated the post_type & taxonomy_name

--- includes/admin/class-cpt.php
+++ includes/admin/class-cpt.php
@@ -7,8 +7,6 @@ namespace Theme_Name\Admin;
 
 use Theme_Name\Traits\Ajax;
 use Theme_Name\Traits\Hooker;
-use Carbon_Fields\Container;
-use Carbon_Fields\Field;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -26,38 +24,37 @@ class CPT {
 	 */
 	public function hooks() {
 		$this->action( 'init', 'register_post_type' );
-		$this->action( 'carbon_fields_register_fields', 'register_custom_fields' );
 	}
 
 	/**
-	 * Register Species post type.
+	 * Register Post_Type post type.
 	 */
 	public function register_post_type() {
 		$labels = [
-			'name'                  => _x( 'Species', 'Post type general name', 'theme_name' ),
-			'singular_name'         => _x( 'Species', 'Post type singular name', 'theme_name' ),
-			'menu_name'             => _x( 'Species', 'Admin Menu text', 'theme_name' ),
-			'name_admin_bar'        => _x( 'Species', 'Add New on Toolbar', 'theme_name' ),
+			'name'                  => _x( 'Post_Type', 'Post type general name', 'theme_name' ),
+			'singular_name'         => _x( 'Post_Type', 'Post type singular name', 'theme_name' ),
+			'menu_name'             => _x( 'Post_Type', 'Admin Menu text', 'theme_name' ),
+			'name_admin_bar'        => _x( 'Post_Type', 'Add New on Toolbar', 'theme_name' ),
 			'add_new'               => __( 'Add New', 'theme_name' ),
-			'add_new_item'          => __( 'Add New Species', 'theme_name' ),
-			'new_item'              => __( 'New Species', 'theme_name' ),
-			'edit_item'             => __( 'Edit Species', 'theme_name' ),
-			'view_item'             => __( 'View Species', 'theme_name' ),
-			'all_items'             => __( 'All Species', 'theme_name' ),
-			'search_items'          => __( 'Search Species', 'theme_name' ),
-			'parent_item_colon'     => __( 'Parent Species:', 'theme_name' ),
-			'not_found'             => __( 'No species found.', 'theme_name' ),
-			'not_found_in_trash'    => __( 'No species found in Trash.', 'theme_name' ),
-			'featured_image'        => _x( 'Species Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'theme_name' ),
+			'add_new_item'          => __( 'Add New Post_Type', 'theme_name' ),
+			'new_item'              => __( 'New Post_Type', 'theme_name' ),
+			'edit_item'             => __( 'Edit Post_Type', 'theme_name' ),
+			'view_item'             => __( 'View Post_Type', 'theme_name' ),
+			'all_items'             => __( 'All Post_Type', 'theme_name' ),
+			'search_items'          => __( 'Search Post_Type', 'theme_name' ),
+			'parent_item_colon'     => __( 'Parent Post_Type:', 'theme_name' ),
+			'not_found'             => __( 'No post_type found.', 'theme_name' ),
+			'not_found_in_trash'    => __( 'No post_type found in Trash.', 'theme_name' ),
+			'featured_image'        => _x( 'Post_Type Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'theme_name' ),
 			'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'theme_name' ),
 			'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'theme_name' ),
 			'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'theme_name' ),
-			'archives'              => _x( 'Species archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'theme_name' ),
-			'insert_into_item'      => _x( 'Insert into species', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'theme_name' ),
-			'uploaded_to_this_item' => _x( 'Uploaded to this species', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'theme_name' ),
-			'filter_items_list'     => _x( 'Filter species list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'theme_name' ),
-			'items_list_navigation' => _x( 'Species list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'theme_name' ),
-			'items_list'            => _x( 'Species list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'theme_name' ),
+			'archives'              => _x( 'Post_Type archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'theme_name' ),
+			'insert_into_item'      => _x( 'Insert into post_type', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'theme_name' ),
+			'uploaded_to_this_item' => _x( 'Uploaded to this post_type', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'theme_name' ),
+			'filter_items_list'     => _x( 'Filter post_type list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'theme_name' ),
+			'items_list_navigation' => _x( 'Post_Type list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'theme_name' ),
+			'items_list'            => _x( 'Post_Type list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'theme_name' ),
 		];
 	 
 		$args = [
@@ -67,7 +64,7 @@ class CPT {
 			'show_ui'            => true,
 			'show_in_menu'       => true,
 			'query_var'          => true,
-			'rewrite'            => [ 'slug' => 'species' ],
+			'rewrite'            => [ 'slug' => 'post_type' ],
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'hierarchical'       => false,
@@ -76,11 +73,6 @@ class CPT {
 			'supports'           => [ 'title', 'editor', 'author', 'thumbnail', 'excerpt' ],
 		];
 	 
-		register_post_type( 'species', $args );
+		register_post_type( 'post_type', $args );
 	}
-
-	/**
-	 * Register Custom fields.
-	 */
-	public function register_custom_fields() {}
 }
--- includes/admin/class-taxonomy.php
+++ includes/admin/class-taxonomy.php
@@ -7,8 +7,6 @@ namespace Theme_Name\Admin;
 
 use Theme_Name\Traits\Ajax;
 use Theme_Name\Traits\Hooker;
-use Carbon_Fields\Container;
-use Carbon_Fields\Field;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -26,7 +24,6 @@ class Taxonomy {
 	 */
 	public function hooks() {
 		$this->action( 'init', 'register_taxonomies' );
-		$this->action( 'carbon_fields_register_fields', 'register_custom_fields' );
 	}
 
 	/**
@@ -41,50 +38,31 @@ class Taxonomy {
 			'species-animal-kingdom'    => esc_html__( 'Animal Kingdom', 'theme_name' ),
 		];
 
-		foreach ( $taxonomies as $taxonomy => $label ) {
-			$labels = [
-				'name'              => $label,
-				'singular_name'     => $label,
-				'search_items'      => sprintf( __( 'Search %s', 'theme_name' ), $label ),
-				'all_items'         => sprintf( __( 'All %s', 'theme_name' ), $label ),
-				'parent_item'       => sprintf( __( 'Parent %s', 'theme_name' ), $label ),
-				'parent_item_colon' => sprintf( __( 'Parent %s:', 'theme_name' ), $label ),
-				'edit_item'         => sprintf( __( 'Edit %s', 'theme_name' ), $label ),
-				'update_item'       => sprintf( __( 'Update %s', 'theme_name' ), $label ),
-				'add_new_item'      => sprintf( __( 'Add New %s', 'theme_name' ), $label ),
-				'new_item_name'     => sprintf( __( 'New %s', 'theme_name' ), $label ),
-				'menu_name'         => $label,
-			];
+		$label  = esc_html__( 'Taxonomy Name', 'theme_name' );
+		$labels = [
+			'name'              => $label,
+			'singular_name'     => $label,
+			'search_items'      => sprintf( __( 'Search %s', 'theme_name' ), $label ),
+			'all_items'         => sprintf( __( 'All %s', 'theme_name' ), $label ),
+			'parent_item'       => sprintf( __( 'Parent %s', 'theme_name' ), $label ),
+			'parent_item_colon' => sprintf( __( 'Parent %s:', 'theme_name' ), $label ),
+			'edit_item'         => sprintf( __( 'Edit %s', 'theme_name' ), $label ),
+			'update_item'       => sprintf( __( 'Update %s', 'theme_name' ), $label ),
+			'add_new_item'      => sprintf( __( 'Add New %s', 'theme_name' ), $label ),
+			'new_item_name'     => sprintf( __( 'New %s', 'theme_name' ), $label ),
+			'menu_name'         => $label,
+		];
 
-			$args = [
-				'hierarchical'      => true,
-				'labels'            => $labels,
-				'show_ui'           => true,
-				'show_admin_column' => true,
-				'query_var'         => true,
-				'show_in_rest'      => true,
-				'rewrite'           => [ 'slug' => $taxonomy ],
-			];
+		$args = [
+			'hierarchical'      => true,
+			'labels'            => $labels,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'show_in_rest'      => true,
+			'rewrite'           => [ 'slug' => 'taxonomy_name' ],
+		];
 
-			register_taxonomy( $taxonomy, [ 'species' ], $args );
-		}
-	}
-
-	/**
-	 * Register Custom fields.
-	 */
-	public function register_custom_fields() {
-		Container::make( 'term_meta', esc_html__( 'Settings', 'theme_name' ) )
-		->where( 'term_taxonomy', '=', 'species-animal-kingdom' )
-		->add_fields(
-			[
-				Field::make( 'text', 'count', esc_html__( 'Species Count', 'theme_name' ) ),
-				Field::make( 'image', 'image', esc_html__( 'Image', 'theme_name' ) ),
-				Field::make( 'text', 'credit', esc_html__( 'Credit', 'theme_name' ) ),
-				Field::make( 'text', 'wiki_url', esc_html__( 'Wiki URL', 'theme_name' ) ),
-				Field::make( 'rich_text', 'subtitle', esc_html__( 'Sub Title', 'theme_name' ) ),
-				Field::make( 'rich_text', 'did_you_know', esc_html__( 'Did You Know?', 'theme_name' ) ),
-			]
-		);
+		register_taxonomy( 'taxonomy_name', [ 'post_type' ], $args );
 	}
 }
